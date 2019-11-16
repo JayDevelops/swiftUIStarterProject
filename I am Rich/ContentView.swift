23d2c7f39eb209fc10311480f7ae4adf3b69ced2
JayDevelops <jaymations1234@gmail.com>
2019-11-15 21:51:32 -0800

    stable build of this UI Swift based application

--- I am Rich/ContentView.swift
+++ I am Rich/ContentView.swift
@@ -10,10 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            // Background color here
+            Color(.systemTeal)
+                // Ignore the safe areas, letting the color fill completely
+                .edgesIgnoringSafeArea(.all)
+            
+            // Embeds the following into a Vertical Stack
+            VStack {
+                // Text label here
+                Text("I am rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                
+                // Resizes the Image for the diamond
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+                
+        }
     }
 }
 
+// This is where you render the actual view
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
